922bfe708a46e2336ad33684778524adf512f112
Sulthan Daffa <Sulthan Daffa@10.252.131.7>
2023-11-16 13:25:15 +0700

    Fix

---
+++ src/Class/Login.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Class;
+
+/**
+ *
+ * @author Sulthan Daffa
+ */
+public class Login {
+    public String username;
+    public String password;
+
+    public Login(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    
+}
