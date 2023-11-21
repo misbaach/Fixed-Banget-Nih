31cf879c52bde3679ca6c7816a7e3f3d28d58b3a
adisigit <adi.sigit13@gmail.com>
2023-11-22 06:46:49 +0700

    tambah class dafttarFilm

---
+++ src/Class/DaftarFilm.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Class;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ASUS
+ */
+public class DaftarFilm {
+    public ArrayList<Film> film;
+    public int jumlahFilm;
+    
+    public DaftarFilm(){
+        film = new ArrayList<>();
+        jumlahFilm = 0;
+    }
+    
+    public void tambahFilm(String nama, String genre, String durasi, String sinopsis, String gambar){
+        if(jumlahFilm < 3){
+            Film filmBaru = new Film(nama, genre, durasi, sinopsis, gambar);
+            film.add(filmBaru);
+            jumlahFilm++;
+        }
+    }
+    
+    public Film pilih(String nama){
+        for(Film cek : film){
+            if(cek.getNama().equals(nama)){
+                return cek;
+            }
+        }
+        return null;
+    }
+    
+    public void hapusFilm(String nama){
+        Film cek = pilih(nama);
+        if(cek != null){
+            film.remove(cek);
+            jumlahFilm--;
+        }
+    }
+    
+    public void editFilm(String nama, String genre, String durasi, String sinopsis, String gambar){
+        Film cek = pilih(nama);
+        if(cek != null){
+            int index = film.indexOf(cek);
+            Film filmBaru = new Film(nama, genre, durasi, sinopsis, gambar);
+            film.set(index, filmBaru);
+        }
+    }
+    
+}
